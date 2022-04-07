077ed9cdd8e60e487e767852a9e992db9bdc0046
nasirshariff619 <nasirshariff97@gmail.com>
2022-04-07 11:11:04 +0000

    adding changes

--- application/__init__.py
+++ application/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
 app.config['SECRET_KEY'] = str(uuid.uuid4)
+#export DATABASE_URI=mysql+pymysql://root:Database1324@10.46.128.3/flask-gcp-db
 
 db = SQLAlchemy(app)
 
--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,23 @@
-from application import app
+from application import app, db
 from application.forms import TaskForm
-from flask import render_template
+from application.models import Tasks
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    all_tasks = Tasks.query.all()
+    return render_template('index.html', all_tasks=all_tasks)
 
 @app.route('/new_task', methods=['GET', 'POST'])
 def new_task():
     form = TaskForm()
+
+    if request.method == "POST":
+        task = Tasks(description=form.description.data)
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for('index'))
+
     return render_template('new_task.html', form=form)
 
 
